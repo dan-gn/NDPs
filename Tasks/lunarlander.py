19140df5c28f75bee7959944944dcbdc731651db
dan-gn <danny@PC-90TR8R3.localdomain>
2026-09-14 10:46:55 +0100

    update

--- Tasks/lunarlander.py
+++ Tasks/lunarlander.py
@@ -50,7 +50,7 @@ LUNARLANDER_PARAMETERS = {
     'creating_threshold': 0,
     'add_edge_strategy': 'all_disconnected',
     # Optimizer parameters
-    'population_size': 512,
+    'population_size': 64,
     'generations': 2000,
     # 'population_size': 10,
     # 'generations': 1,
